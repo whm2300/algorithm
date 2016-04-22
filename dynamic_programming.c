2c437af37359d7c2e859310ff371e3707ee1e2ad
unknown <MichaelChen@boyaa.com>
2016-04-22 19:14:04 +0800

    dp 问题

--- dynamic_programming.c
+++ dynamic_programming.c
@@ -19,7 +19,6 @@ int CutRod(int *p, int n) {
 }
 
 //带备忘录的自顶向下法
-
 int MemoizedCutRodAux(int *p, int n, int *r) {
     if (r[n] >= 0) {
         return r[n];
@@ -31,7 +30,7 @@ int MemoizedCutRodAux(int *p, int n, int *r) {
     } else {
         q = -1;
         int i;
-        for (i = 1; i < n; ++i) {
+        for (i = 1; i <= n; ++i) {
             q = max(q, p[i]+MemoizedCutRodAux(p, n-i, r));
         }
     }
@@ -41,14 +40,29 @@ int MemoizedCutRodAux(int *p, int n, int *r) {
 int MemoizedCutRod(int *p, int n) {
     int r[n];
     int i;
-    for (i = 0; i < n; ++i) {
-        r[i] = -1;
+    for (i = 0; i <= n; ++i) {
+        r[i] = -1000;
     }
     return MemoizedCutRodAux(p, n, r);
 }
 
+//自底向上
+int BottomUpCutRod(int *p, int n) {
+    int r[n];
+    int i;
+    int j;
+    r[0] = 0;
+    for (j = 1; j <= n; ++j) {
+        int q = -1;
+        for (i = 1; i <= j; ++i) {
+            q = max(q, p[i]+r[j-i]);
+        }
+        r[j] = q;
+    }
+    return r[n];
+}
+
 int main(int argc, char *argv[]) {
-    int ret;
     int i;
     //int p[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
     int p[64];
@@ -57,8 +71,9 @@ int main(int argc, char *argv[]) {
     }
     for (i = 1; i < 64; ++i) {
         //int ret = CutRod(p, i);
-        int ret = MemoizedCutRod(p, i);
-        printf("i%d, r:%d\n", i, ret);
+        //int ret = MemoizedCutRod(p, i);
+        int ret = BottomUpCutRod(p, i);
+        printf("i:%d, r:%d\n", i, ret);
     }
     return 0;
 }
